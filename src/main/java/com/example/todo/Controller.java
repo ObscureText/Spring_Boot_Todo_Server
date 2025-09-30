4937c3e307a6e5db255297a515b8afa99c314d68
Favas K P <183742743+favas-kp@users.noreply.github.com>
2025-09-30 14:18:38 +0530

    added delete account instructions

--- src/main/java/com/example/todo/Controller.java
+++ src/main/java/com/example/todo/Controller.java
@@ -10,50 +10,50 @@ public class Controller {
 
     @GetMapping("/")
     public String root() {
-        return "Hello world";
+        return "Hello there ..!";
     }
 
-    @GetMapping("/greet")
-    public String greet(@RequestParam String name) {
-        return "Hello - " + name;
-    }
+//    @GetMapping("/greet")
+//    public String greet(@RequestParam String name) {
+//        return "Hello - " + name;
+//    }
 
-    @PostMapping("/add")
-    public String addTask(@RequestBody String task) {
-        return "Adds - " + task;
-    }
+//    @PostMapping("/add")
+//    public String addTask(@RequestBody String task) {
+//        return "Adds - " + task;
+//    }
 
-    @PostMapping("/tasks")
-    public List<Map<String, String>> postTasks(@RequestBody String userId) {
-        List<Map<String, String>> tasks =  List.of(
-            Map.of("taskId", "1","userId", "1", "title", "For user one - 1"),
-            Map.of("taskId", "2","userId", "1", "title", "For user one - 2"),
-            Map.of("taskId", "3","userId", "1", "title", "For user one - 3"),
-            Map.of("taskId", "4","userId", "2", "title", "For user two - 1"),
-            Map.of("taskId", "5","userId", "2", "title", "For user two - 2"),
-            Map.of("taskId", "6","userId", "2", "title", "For user two - 3"),
-            Map.of("taskId", "7","userId", "3", "title", "For user three - 1"),
-            Map.of("taskId", "8","userId", "3", "title", "For user three - 2"),
-            Map.of("taskId", "9","userId", "3", "title", "For user three - 3")
-        );
+//    @PostMapping("/tasks")
+//    public List<Map<String, String>> postTasks(@RequestBody String userId) {
+//        List<Map<String, String>> tasks =  List.of(
+//            Map.of("taskId", "1","userId", "1", "title", "For user one - 1"),
+//            Map.of("taskId", "2","userId", "1", "title", "For user one - 2"),
+//            Map.of("taskId", "3","userId", "1", "title", "For user one - 3"),
+//            Map.of("taskId", "4","userId", "2", "title", "For user two - 1"),
+//            Map.of("taskId", "5","userId", "2", "title", "For user two - 2"),
+//            Map.of("taskId", "6","userId", "2", "title", "For user two - 3"),
+//            Map.of("taskId", "7","userId", "3", "title", "For user three - 1"),
+//            Map.of("taskId", "8","userId", "3", "title", "For user three - 2"),
+//            Map.of("taskId", "9","userId", "3", "title", "For user three - 3")
+//        );
+//
+//        return tasks.stream().filter(task -> task.get("userId").equals(userId)).toList();
+//    }
 
-        return tasks.stream().filter(task -> task.get("userId").equals(userId)).toList();
-    }
-
-    @GetMapping("/tasks")
-    public List<Map<String, String>> getTasks(@RequestParam String userId) {
-        List<Map<String, String>> tasks =  List.of(
-                Map.of("taskId", "1","userId", "1", "title", "For user one - 1"),
-                Map.of("taskId", "2","userId", "1", "title", "For user one - 2"),
-                Map.of("taskId", "3","userId", "1", "title", "For user one - 3"),
-                Map.of("taskId", "4","userId", "2", "title", "For user two - 1"),
-                Map.of("taskId", "5","userId", "2", "title", "For user two - 2"),
-                Map.of("taskId", "6","userId", "2", "title", "For user two - 3"),
-                Map.of("taskId", "7","userId", "3", "title", "For user three - 1"),
-                Map.of("taskId", "8","userId", "3", "title", "For user three - 2"),
-                Map.of("taskId", "9","userId", "3", "title", "For user three - 3")
-        );
-
-        return tasks.stream().filter(task -> task.get("userId").equals(userId)).toList();
-    }
+//    @GetMapping("/tasks")
+//    public List<Map<String, String>> getTasks(@RequestParam String userId) {
+//        List<Map<String, String>> tasks =  List.of(
+//                Map.of("taskId", "1","userId", "1", "title", "For user one - 1"),
+//                Map.of("taskId", "2","userId", "1", "title", "For user one - 2"),
+//                Map.of("taskId", "3","userId", "1", "title", "For user one - 3"),
+//                Map.of("taskId", "4","userId", "2", "title", "For user two - 1"),
+//                Map.of("taskId", "5","userId", "2", "title", "For user two - 2"),
+//                Map.of("taskId", "6","userId", "2", "title", "For user two - 3"),
+//                Map.of("taskId", "7","userId", "3", "title", "For user three - 1"),
+//                Map.of("taskId", "8","userId", "3", "title", "For user three - 2"),
+//                Map.of("taskId", "9","userId", "3", "title", "For user three - 3")
+//        );
+//
+//        return tasks.stream().filter(task -> task.get("userId").equals(userId)).toList();
+//    }
 }
